f5493751b1276b7cef312902d4fb3bb87b3f16a8
HugoDhugo <hugo.deduit@gmail.com>
2024-12-23 11:23:45 +0100

    ajout comments

--- back/agent.py
+++ back/agent.py
@@ -27,31 +27,27 @@ async def entrypoint(ctx: JobContext):
         text=(
             "Tu es un assistant vocal pour personnes atteintes de la maladie d'alzheimer. Ton interface avec les utilisateurs sera la voix. "
             "Tu dois utiliser des réponses courtes et consises, et éviter d'utiliser des mots ou de la ponctuation difficile à prononcer. "
-        ),
+        ), # Contexte à donner à chatgpt. Problème, il semblerait qu'il  faut redonner le contexte à chaque fois.
+           # Cela risque d'être plus couteux en terme de token openai
     )
 
     logger.info(f"connecting to room {ctx.room.name}")
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
 
-    # Wait for the first participant to connect
     participant = await ctx.wait_for_participant()
     logger.info(f"starting voice assistant for participant {participant.identity}")
 
-    # This project is configured to use Deepgram STT, OpenAI LLM and TTS plugins
-    # Other great providers exist like Cartesia and ElevenLabs
-    # Learn more and pick the best one for your app:
-    # https://docs.livekit.io/agents/plugins
     agent = VoicePipelineAgent(
-        vad=ctx.proc.userdata["vad"],
-        stt=deepgram.STT(language='fr'),
-        llm=openai.LLM(model="gpt-4o-mini"),
-        tts=openai.TTS(),
+        vad=ctx.proc.userdata["vad"], # On utilise silero, pas le choix
+        stt=deepgram.STT(language='fr'), # On utilise deepgram, peut être changé
+        llm=openai.LLM(model="gpt-4o-mini"), # Il serait idéal d'utiliser un llm en local
+        tts=openai.TTS(),                    #   ^^ same
         chat_ctx=initial_ctx,
     )
 
     agent.start(ctx.room, participant)
 
-    # The agent should be polite and greet the user when it joins :)
+    # Phrase d'acceuil quand l'utilisateur se connecte
     await agent.say("Salutations l'ancien, comment je peux t'aider ?", allow_interruptions=True)
 
 
